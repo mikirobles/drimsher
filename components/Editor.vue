ff55440603f5378257691e813ad1cb8843324f36
Micael Robles <micael.robles@gmail.com>
2018-02-14 09:47:30 -0300

    editor component

---
+++ components/Editor.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <textarea v-model="editorText" type="text" placeholder="Escribe aquí tu sueño." />
+        <p v-bind:class="exceededLimit ? 'exceeded' : ''">{{ charsLeft }}</p>
+    </div>
+</template>
+
+<script>
+const charsLimit = 450;
+export default {
+  data: () => ({
+    editorText: ""
+  }),
+  computed: {
+    charsLeft() {
+      return `${this.editorText.length} / ${charsLimit}`;
+    },
+    exceededLimit() {
+      return this.editorText.length > charsLimit;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.exceeded {
+  background-image: linear-gradient(-180deg, #ff0303 0%, #bd1313 100%);
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,73 @@
 <template>
   <section class="container">
     <header>
-      <img src="icons/refresh.svg" alt="">
+      <div class="buttons">
+        <img key="refresh" v-if="!isEditor" src="icons/refresh.svg" alt="refresh">
+        <img key="back" v-on:click="isEditor = !isEditor" v-else src="icons/back.svg" alt="go back">
+        <img v-bind:class="isEditor ? 'hidden' : ''" v-on:click="isEditor = !isEditor" src="icons/write.svg" alt="">
+      </div>
       <h1 class="logo">
         drimsher
       </h1>
-      <img v-on:click="isEditor = !isEditor" src="icons/write.svg" alt="">
     </header>
     <transition name="fade" mode="out-in">
       <article key="dreamview" v-if="!isEditor">
           <p>{{dream}}</p>
       </article>
       <article key="editor" v-else>
-        <textarea v-model="editorText" type="text" placeholder="Escribe aquí tu sueño." />
+        <editor @update-editor-text="(text) => {editorText = text}" />
       </article>
     </transition>
   </section>
 </template>
 
 <script>
+import Editor from "../components/Editor";
 export default {
   head: {
     title: "drimsher"
   },
+  components: {
+    Editor
+  },
   data: () => ({
-    dream: "Lorem ipsum dolor sit amet, at alienum fastidii vel, eu ius elit congue. Ad nam reque adolescens honestatis, per ex nostrud euripidis. Vim vidit disputando ei. Ad his ipsum reprimique, ex quis posidonium nam. \n Quando utinam delicatissimi nam cu, clita indoctum mei in. Harum urbanitas his eu, sit legere quaestio adipiscing ea. Quando utinam delicatissimi nam cu, clita indoctum mei in. Harum urbanitas his eu, sit legere quaestio adipiscing ea.",
-    isEditor: false,
-    editorText: ""
+    dream:
+      "Lorem ipsum dolor sit amet, at alienum fastidii vel, eu ius elit congue. Ad nam reque adolescens honestatis, per ex nostrud euripidis. Vim vidit disputando ei. Ad his ipsum reprimique, ex quis posidonium nam. \n Quando utinam delicatissimi nam cu, clita indoctum mei in. Harum urbanitas his eu, sit legere quaestio adipiscing ea. Quando utinam delicatissimi nam cu, clita indoctum mei in. Harum urbanitas his eu, sit legere quaestio adipiscing ea.",
+    isEditor: false
   })
-}
+};
 </script>
 
 <style lang="scss">
 @font-face {
-    font-family: 'Avenir';
-    src: url('../static/fonts/AvenirNextLTPro-Regular.woff2') format('woff2'),
-        url('../static/fonts/AvenirNextLTPro-Regular.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
+  font-family: "Avenir";
+  src: url("../static/fonts/AvenirNextLTPro-Regular.woff2") format("woff2"),
+    url("../static/fonts/AvenirNextLTPro-Regular.woff") format("woff");
+  font-weight: normal;
+  font-style: normal;
 }
 
 * {
-  font-family: 'Avenir', sans-serif;
+  font-family: "Avenir", sans-serif;
   line-height: 1.5;
   font-size: 18px;
 }
 
 header {
-  display: flex;
-  padding: 1em 0;
+  position: relative;
+  margin: 1em 0;
   width: 100%;
-  justify-content: space-between;
+  .logo {
+    text-align: center;
+  }
+  .buttons {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 }
 .container {
   min-height: 100vh;
@@ -66,18 +83,18 @@ header {
 .logo {
   font-size: 2em;
   letter-spacing: 0.17px;
-  background-image: linear-gradient(-180deg, #4BC3FD 0%, #B224FA 100%);
+  background-image: linear-gradient(-180deg, #4bc3fd 0%, #b224fa 100%);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
 article {
   font-weight: bold;
   width: 100%;
-  min-height: calc( 100vh - 90px );
+  min-height: calc(100vh - 90px);
   padding-top: 5vh;
   p {
     white-space: pre-line;
-    background-image: linear-gradient(-180deg, #550B39 0%, #640993 100%);
+    background-image: linear-gradient(-180deg, #550b39 0%, #640993 100%);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
   }
@@ -89,17 +106,22 @@ textarea {
   padding: 0.5em;
   border: 0;
   resize: none;
-  color: #550B39;
+  color: #550b39;
   &:focus {
     outline: none;
   }
 }
 
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+.hidden {
   opacity: 0;
 }
 
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
